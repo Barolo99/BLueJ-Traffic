2bed4b749db30c7e305455721d0413273f19e526
andersvinde <andersvinde@gmail.com>
2017-11-06 16:07:52 +0100

    :+1:

--- src/billetautomat2/Billet1Screen.java
+++ src/billetautomat2/Billet1Screen.java
@@ -2,24 +2,34 @@ package billetautomat2;
 
 import javax.swing.JPanel;
 import javax.swing.JButton;
+import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import javax.swing.JComboBox;
 
 public class Billet1Screen extends JPanel {
 
 	/**
 	 * Create the panel.
+	 * Hej med dig
 	 */
 	public Billet1Screen() {
 		setLayout(null);
-		
+		String[] comboBoxString = {"Zone 1","Zone 2","Zone 3"};
+		JComboBox comboBox = new JComboBox(comboBoxString);
+		comboBox.setToolTipText("V\u00E6lg zone");
+		comboBox.setBounds(185, 17, 97, 26);
+		add(comboBox);
+		String value=(String)comboBox.getSelectedItem();
+		System.out.println(value);
 		JButton btnTest = new JButton("Køb");
 		btnTest.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				
+				String value=(String)comboBox.getSelectedItem();
+				System.out.println(value);
 			}
 		});
 		btnTest.addActionListener(new ActionListener() {
@@ -28,7 +38,7 @@ public class Billet1Screen extends JPanel {
 		});
 		btnTest.setBounds(15, 16, 115, 29);
 		add(btnTest);
-
+		
+		
 	}
-
 }
---
+++ src/billetautomat2/Login.java
@@ -0,0 +1,63 @@
+package billetautomat2;
+
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JPasswordField;
+import java.awt.Component;
+import javax.swing.Box;
+
+public class Login extends JPanel {
+	private JTextField textField_1;
+	private JPasswordField passwordField;
+
+	/**
+	 * Create the panel.
+	 */
+	public Login() {
+		setLayout(null);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(301, 111, 86, 20);
+		add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblUsername = new JLabel("Username:");
+		lblUsername.setBounds(202, 114, 73, 14);
+		add(lblUsername);
+		
+		JLabel lblPassword = new JLabel("Password:");
+		lblPassword.setBounds(202, 157, 73, 14);
+		add(lblPassword);
+		
+		JLabel lblBluejtraffic = new JLabel("BlueJ-Traffic");
+		lblBluejtraffic.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		lblBluejtraffic.setBounds(227, 45, 167, 31);
+		add(lblBluejtraffic);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(301, 154, 86, 20);
+		add(passwordField);
+		
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.setBounds(301, 203, 86, 23);
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				String uname =textField_1.getText(); 
+				String pass=passwordField.getText();
+				
+				System.out.println(pass);
+				System.out.println(uname);
+				
+			}
+		});
+		add(btnLogin);
+
+	}
+}
